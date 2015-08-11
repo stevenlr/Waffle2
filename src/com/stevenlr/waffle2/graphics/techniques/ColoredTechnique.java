cc4f2590029663e1725d22fa5f2e4e482fd8ba13
Steven Le Rouzic <steven.lerouzic@gmail.com>
2015-08-12 00:10:20 +0200

    reduced colored square rendering to use 1 vbo only

--- src/com/stevenlr/waffle2/graphics/techniques/ColoredTechnique.java
+++ src/com/stevenlr/waffle2/graphics/techniques/ColoredTechnique.java
@@ -8,11 +8,9 @@ import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL13.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
-import static org.lwjgl.opengl.GL31.*;
 import static org.lwjgl.opengl.GL33.*;
 
 public class ColoredTechnique {
@@ -22,14 +20,13 @@ public class ColoredTechnique {
 	private static final int TRANSFORM_LOCATION = 1;
 	private static final int COLOR_LOCATION = 4;
 	private static final int BUFFER_SIZE = 1024;
+	private static final int ELEMENT_SIZE = 13;
 
-	private MultiFloatBuffer _transforms = new MultiFloatBuffer(9 * BUFFER_SIZE);
-	private MultiFloatBuffer _colors = new MultiFloatBuffer(4 * BUFFER_SIZE);
+	private MultiFloatBuffer _buffer = new MultiFloatBuffer(ELEMENT_SIZE * BUFFER_SIZE);
 	private Quad _quad;
 
 	private int _vao;
-	private int _vboTransforms;
-	private int _vboColors;
+	private int _vbo;
 
 	public static void init() {
 		_coloredShader = new Shader("/shaders/colored.vert", "/shaders/colored.frag");
@@ -48,24 +45,22 @@ public class ColoredTechnique {
 		glEnableVertexAttribArray(POSITION_LOCATION);
 		quad.bindVertexAttrib(POSITION_LOCATION);
 
-		_vboTransforms = glGenBuffers();
-		GLStates.bindBufferArray(_vboTransforms);
-		glBufferData(GL_ARRAY_BUFFER, _transforms.getByteSize(), GL_DYNAMIC_DRAW);
+		_vbo = glGenBuffers();
+		GLStates.bindBufferArray(_vbo);
+		glBufferData(GL_ARRAY_BUFFER, _buffer.getByteSize(), GL_DYNAMIC_DRAW);
+
 		glEnableVertexAttribArray(TRANSFORM_LOCATION);
 		glEnableVertexAttribArray(TRANSFORM_LOCATION + 1);
 		glEnableVertexAttribArray(TRANSFORM_LOCATION + 2);
-		glVertexAttribPointer(TRANSFORM_LOCATION, 3, GL_FLOAT, false, 9 * 4, 0);
-		glVertexAttribPointer(TRANSFORM_LOCATION + 1, 3, GL_FLOAT, false, 9 * 4, 3 * 4);
-		glVertexAttribPointer(TRANSFORM_LOCATION + 2, 3, GL_FLOAT, false, 9 * 4, 6 * 4);
+		glVertexAttribPointer(TRANSFORM_LOCATION, 3, GL_FLOAT, false, ELEMENT_SIZE * 4, 0);
+		glVertexAttribPointer(TRANSFORM_LOCATION + 1, 3, GL_FLOAT, false, ELEMENT_SIZE * 4, 3 * 4);
+		glVertexAttribPointer(TRANSFORM_LOCATION + 2, 3, GL_FLOAT, false, ELEMENT_SIZE * 4, 6 * 4);
 		glVertexAttribDivisor(TRANSFORM_LOCATION, 1);
 		glVertexAttribDivisor(TRANSFORM_LOCATION + 1, 1);
 		glVertexAttribDivisor(TRANSFORM_LOCATION + 2, 1);
 
-		_vboColors = glGenBuffers();
-		GLStates.bindBufferArray(_vboColors);
-		glBufferData(GL_ARRAY_BUFFER, _colors.getByteSize(), GL_DYNAMIC_DRAW);
 		glEnableVertexAttribArray(COLOR_LOCATION);
-		glVertexAttribPointer(COLOR_LOCATION, 4, GL_FLOAT, false, 0, 0);
+		glVertexAttribPointer(COLOR_LOCATION, 4, GL_FLOAT, false, ELEMENT_SIZE * 4, 9 * 4);
 		glVertexAttribDivisor(COLOR_LOCATION, 1);
 
 		GLStates.bindBufferArray(0);
@@ -73,11 +68,11 @@ public class ColoredTechnique {
 	}
 
 	public void add(Matrix3f transform, float r, float g, float b, float a) {
-		_transforms.put(transform);
-		_colors.put(r);
-		_colors.put(g);
-		_colors.put(b);
-		_colors.put(a);
+		_buffer.put(transform);
+		_buffer.put(r);
+		_buffer.put(g);
+		_buffer.put(b);
+		_buffer.put(a);
 	}
 
 	public void doRenderPass(Matrix4f projection) {
@@ -86,25 +81,21 @@ public class ColoredTechnique {
 		GLStates.bindVertexArray(_vao);
 
 		while (true) {
-			MultiFloatBuffer.ReadInfo readTransforms = _transforms.read();
-			MultiFloatBuffer.ReadInfo readColors = _colors.read();
+			MultiFloatBuffer.ReadInfo readBuffer = _buffer.read();
 
-			if (readTransforms == null || readColors == null) {
+			if (readBuffer == null) {
 				break;
 			}
 
-			GLStates.bindBufferArray(_vboTransforms);
-			glBufferSubData(GL_ARRAY_BUFFER, 0, readTransforms.buffer);
-			GLStates.bindBufferArray(_vboColors);
-			glBufferSubData(GL_ARRAY_BUFFER, 0, readColors.buffer);
+			GLStates.bindBufferArray(_vbo);
+			glBufferSubData(GL_ARRAY_BUFFER, 0, readBuffer.buffer);
 			GLStates.bindBufferArray(0);
 
-			_quad.drawInstanced(readColors.size / 4);
+			_quad.drawInstanced(readBuffer.size / ELEMENT_SIZE);
 		}
 	}
 
 	public void reset() {
-		_transforms.reset();
-		_colors.reset();
+		_buffer.reset();
 	}
 }
